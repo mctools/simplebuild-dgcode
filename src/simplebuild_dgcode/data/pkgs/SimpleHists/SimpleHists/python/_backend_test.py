1920de5105503317965eac71d8d0ada7c59054b5
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2024-02-13 13:02:57 +0100

    Try to allow macosx backend

--- src/simplebuild_dgcode/data/pkgs/SimpleHists/SimpleHists/python/_backend_test.py
+++ src/simplebuild_dgcode/data/pkgs/SimpleHists/SimpleHists/python/_backend_test.py
@@ -2,7 +2,7 @@ import matplotlib
 
 #we want to move from gtkagg to tkagg, but if gtkagg is present we allow it (since it works - at least for now).
 _backend = matplotlib.get_backend().lower()
-_backend_ok = (_backend in ('tkagg','gtkagg','agg'))
+_backend_ok = (_backend in ('tkagg','gtkagg','agg','macosx'))
 
 if _backend_ok and _backend=='gtkagg':
     #attempt to silence some useless/confusing warnings
@@ -22,7 +22,7 @@ if _backend_ok and _backend=='tkagg':
                                          ' for now, the API will likely change in version 2.1'))
 
 from PyAna.fpe import standardMPLFixes as _standardMPLFixes # noqa E402
-_standardMPLFixes
+_standardMPLFixes()
 
 def _ensure_backend_ok():
     if _backend_ok:
@@ -62,7 +62,7 @@ def _ensure_backend_ok():
     ###                                                                                                      ###
     ###         If the test for tkagg does not give "all ok", then you might have to reinstall matplotlib    ###
     ###         after installing tcl/tk headers. Alternatively you can try the "gtkagg" backend which        ###
-    ###         requires gtk and pygtk to be installed.                                                      ###
+    ###         requires gtk and pygtk to be installed. On MacOS you might want to use "macosx"              ###
     ###                                                                                                      ###
     ############################################################################################################
     ############################################################################################################
