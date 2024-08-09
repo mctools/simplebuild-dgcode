f1a6bd4e0d89473f5a953b938f503c1dcf0acf58
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2024-08-09 16:59:23 +0200

    Fix cached_noarch_definitions.py

--- .github/resources/cached_noarch_definitions.py
+++ .github/resources/cached_noarch_definitions.py
@@ -14,7 +14,16 @@ geant4-data-realsurface =2.2.0
 geant4-data-saiddata =2.0.0
 """
 
-cached_noarch_pkgs = [ l.strip().split('=') for l in cached_noarch_pkgs_def.splitlines() if l.strip() ]
+def _parse_line(line):
+    if not line.strip():
+        return
+    name,version = line.strip().split('=')
+    return name.strip(),version.strip()
+
+cached_noarch_pkgs = [ _parse_line(line)
+                       for line in cached_noarch_pkgs_def.splitlines() ]
+cached_noarch_pkgs = [ e for e in cached_noarch_pkgs if e is not None ]
+
 
 def produce_yml():
     print("""name: cached_noarch_pkgs
@@ -37,12 +46,14 @@ def verify_active_env_use_correct_versions( allow_no_usage = False ):
     conda_list = json.loads(cl.stdout)
     for pkg in conda_list:
         pn, pv = pkg['name'], pkg['version']
-        l = [ expected_version for name, expected_version in cached_noarch_pkgs if name == pn ]
-        if len(l) != 1:
+        ll = [ expected_version for name, expected_version in cached_noarch_pkgs if name == pn ]
+        if len(ll) != 1:
             raise SystemExit(f'Unexpected error: package {pn} was found in the current'
                              ' conda env but does not appear exactly once in the definition. This should never happen.')
-        expected_version = l[0]
-        if expected_version != pv:
+        expected_version = ll[0]
+        version_ok = ( expected_version == pv
+                       or expected_version.endswith('.0') and expected_version[:-2]==pv )#allow '3.3' == '3.3.0'
+        if not version_ok:
             raise SystemExit(f'Error: Active conda env has unexpected package version of package {pn} (found {pv} but'
                              f' expected {expected_version}). Time to update the hardwired cache definition?')
         print(f'  --> Verified usage of {pn}={pv}')
@@ -51,7 +62,7 @@ def verify_active_env_use_correct_versions( allow_no_usage = False ):
         for pn,pv in cached_noarch_pkgs:
             if not any( pkg['name']==pn for pkg in conda_list ):
                 raise SystemExit(f'Error: Active conda env did not use cached package {pn}. Time to update the hardwired cache definition?')
-    print(f'  --> Verified all usages.')
+    print('  --> Verified all usages.')
 
 if __name__=='__main__':
     import sys
